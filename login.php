f2de5bee666d087448561c78fa76d6d7b5ce407f
Yordan Radkov <yorradgeo@alu.edu.gva.es>
2024-11-13 13:23:15 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,17 @@
 <?php
 $user = $_POST["user"];
 $password = $_POST["password"];
+?>
 
-echo "Tu usuario es ".$user." y tu contraseña es ".$password;
+/*echo "Tu usuario es ".$user." y tu contraseña es ".$password;*/
+
+<html>
+  <table border="1";>
+    <tr>Usuari</tr>
+    <td><?php $user ?></td>
+
+    <tr>Contraseña</tr>
+    <td><?php $password ?></td>
+  </table>
+
+</html>
